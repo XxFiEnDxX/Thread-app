8aaeaa82a715bf703f7c11b90b7ffc4c425c1849
Amit Kumar Singh <91126008+XxFiEnDxX@users.noreply.github.com>
2024-09-14 13:16:54 +0530

    created .env for backend server

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,18 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3000,
-    // get rid of the post errors
-    proxy: {
-      "/api":{
-        target: "http://localhost:5000",
-        changeOrigin: true,
-        secure:false,
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd()); // Load environment variables
+
+  return {
+    plugins: [react()],
+    server: {
+      port: 3000,
+      proxy: {
+        '/api': {
+          target: env.VITE_BACKEND_URI, // Use the loaded env variable
+          changeOrigin: true,
+          secure: false,
+        },
       },
-    }
-  }
-})
+    },
+  };
+});
